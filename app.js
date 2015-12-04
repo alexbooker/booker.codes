53e121bb2bb88e718c4396a2cf3d6707335c2dc9
Alex Booker <alexbooker@fastmail.im>
2015-12-04 10:40:15 +0000

    added nav toggle behaviour

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 require("normalize.css/normalize.css");
 require("./app.scss");
+
+const btn = document.getElementById("burger-btn");
+btn.onclick = function () {
+  document
+    .querySelector(".nav")
+    .classList
+    .toggle("nav--expanded");
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  entry: './app.js',
+  entry: "./app.js",
   output: {
-    filename: 'bundle.js'       
+    filename: "bundle.js"       
   },
   module: {
     loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel",
+        query: {
+          presets: ["es2015"]
+        }
+      },
       {
         test: /\.s?css$/,
         loader: "style-loader!css-loader!sass-loader"
